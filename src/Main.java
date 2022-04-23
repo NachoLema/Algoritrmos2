4642c3818277bc942dd5f4d9a52efe0698cf2c19
Nacho <86265445+NachoLema@users.noreply.github.com>
2022-04-23 14:58:16 -0300

    mipush

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@ public class Main{
 public static void main(String[]args){
 
         NodoAB subIzq=new NodoAB(2,null,null);
+
+
+
         NodoAB subDer=new NodoAB(3,null,null);
+
+
         NodoAB raiz=new NodoAB(1,subIzq,subDer);
 
 
